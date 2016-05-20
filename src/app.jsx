64f463b4a689298c0ca9a2d47374dd11a370c8ce
Suthon <suthon@gmail.com>
2016-05-21 03:47:43 +0700

    for push

--- src/app.jsx
+++ src/app.jsx
@@ -2,8 +2,28 @@ var React = require('react')
 var ReactDOM = require('react-dom');
 import { createStore } from 'redux'
 
+function counter(state = 0, action){
+	if(action.type === 'INCREMENT'){
+		state = state + 1;
+	}
+	return state;
+}
 
-var App = React.createClass({
+
+const store = createStore(counter);
+
+const render = () =>{
+	document.body.innerText = store.getState()
+}
+
+store.subscribe(render);
+render();
+
+document.addEventListener('click', ()=>{
+	store.dispatch({type: 'INCREMENT'})
+})
+
+/*var App = React.createClass({
 	render: function(){
 		return (<h3>Hello World</h3>);
 	}
@@ -12,4 +32,4 @@ var App = React.createClass({
 ReactDOM.render(
   <App/>,
   document.getElementById('app')
-);
+);*/
